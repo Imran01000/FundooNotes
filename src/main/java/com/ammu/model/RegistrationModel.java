ad005a065359630c061de06571c65b4e448998a6
Imran01000 <imshaikh01000@gmail.com>
2020-04-14 12:09:23 +0530

    ADDED:Added registration pojo class

---
+++ src/main/java/com/ammu/model/RegistrationModel.java
@@ -0,0 +1,34 @@
+package com.ammu.model;
+
+public class RegistrationModel 
+{
+	private String fname;
+	private String lname;
+	private String mail;
+	private String password;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
