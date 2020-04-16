ea6565f6bf42b987246486649e09e0cbacff5a08
Imran01000 <imshaikh01000@gmail.com>
2020-04-16 11:50:03 +0530

    MODIFIED:Added post mapping(/add) to check the connection with database

--- src/main/java/com/ammu/controller/AppController.java
+++ src/main/java/com/ammu/controller/AppController.java
@@ -1,21 +1,26 @@
 package com.ammu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ammu.model.LoginModel;
 import com.ammu.model.RegistrationModel;
+import com.ammu.repository.UserRepository;
 
 @RestController
+@RequestMapping("/user")  // http://localhost:8082/user
 public class AppController 
 {
 	@RequestMapping("/login")
-	public LoginModel login(@RequestParam(value="name") String name)
+	public LoginModel login(@RequestParam(value="name")String name1,String password)
 	{
 		LoginModel loginModel = new LoginModel();
-		loginModel.setUserName("Your name is  "+name );
-		loginModel.setPassword("98765");
+		loginModel.setUserName(name1);
+		loginModel.setPassword(password);
 		return loginModel;
 	}
 	
@@ -29,4 +34,21 @@ public class AppController
 		registrationModel.setPassword("45678987");
 		return registrationModel;
 	}
+	
+	@Autowired
+	UserRepository user;
+	
+	@PostMapping(path="/add")
+	@ResponseBody
+	public String addLoginUser(@RequestParam(value="name") String name,@RequestParam(value="pwd") String password)
+	{
+		LoginModel loginModel = new LoginModel();
+		loginModel.setUserName(name);
+		loginModel.setPassword(password);
+		user.save(loginModel);
+		return "saved";
+	}
+	
+	
+	
 }
