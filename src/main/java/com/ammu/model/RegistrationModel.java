5284c75a9d4408b83e5039752774c5c199043416
Imran01000 <imshaikh01000@gmail.com>
2020-04-17 14:13:38 +0530

    MODIFIED:Added some jpa annotation with their attributes

--- src/main/java/com/ammu/model/RegistrationModel.java
+++ src/main/java/com/ammu/model/RegistrationModel.java
@@ -1,11 +1,40 @@
 package com.ammu.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.ammu.dto.RegistrationDto;
+
+@Entity
+@Table(name = "user_data")
 public class RegistrationModel 
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "first_name" , nullable = false ,unique = true)
 	private String fname;
+	
+	@Column(name = "last_name" , nullable = false)
 	private String lname;
+	
+	@Column(name = "mail" , nullable = false , unique = true)
 	private String mail;
+	
+	@Column(name = "pwd" , nullable = false)
 	private String password;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getFname() {
 		return fname;
 	}
