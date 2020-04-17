35175929c612d5a475e3bbac6b3e1cce49529a4d
Imran01000 <imshaikh01000@gmail.com>
2020-04-17 14:11:28 +0530

    ADDED:Added dto class for registration details

---
+++ src/main/java/com/ammu/dto/RegistrationDto.java
@@ -0,0 +1,35 @@
+package com.ammu.dto;
+
+public class RegistrationDto 
+{
+	private String fname;
+	private String lname;
+	private String mail;
+	private String password;
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
