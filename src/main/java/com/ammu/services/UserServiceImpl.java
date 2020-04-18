3316bc8d8a1d15e7959598b923aca574c4a09ac9
Imran01000 <imshaikh01000@gmail.com>
2020-04-18 15:38:36 +0530

    MODIFIED:Added implementation class

--- src/main/java/com/ammu/services/UserServiceImpl.java
+++ src/main/java/com/ammu/services/UserServiceImpl.java
@@ -1,27 +1,37 @@
 package com.ammu.services;
 
-import java.util.Optional;
-
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ammu.dto.LoginDto;
+import com.ammu.dto.RegistrationDto;
+import com.ammu.model.LoginModel;
 import com.ammu.model.RegistrationModel;
 import com.ammu.repository.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService
 {
+
 	@Autowired
-	private UserRepository userRepository;
+	UserRepository userRepo;
 
 	@Override
-	public Optional<RegistrationModel> findUserByEmail(String mail) 
+	public String loginUser(LoginDto loginDto)
 	{
-		return userRepository.findByEmail(mail);
+		LoginModel model = new LoginModel();
+		ModelMapper mapper = new ModelMapper();
+		LoginDto dto = new LoginDto();
+		userRepo.save(model);
+		mapper.map(dto, model);
+		return "Successfully save";
 	}
 
-	@Override
-	public void save(RegistrationModel model)
-	{
-		userRepository.save(model);
-	}
+
+
+
+
+
 
 }
