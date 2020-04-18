a9c37c8fd6060f67cff582271065a20c68e00da1
Imran01000 <imshaikh01000@gmail.com>
2020-04-18 15:35:28 +0530

    MODIFIED:Added loginModel class to just check its properly working or not

--- src/main/java/com/ammu/repository/UserRepository.java
+++ src/main/java/com/ammu/repository/UserRepository.java
@@ -1,14 +1,11 @@
 package com.ammu.repository;
 
-import java.util.Optional;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
+import com.ammu.model.LoginModel;
 import com.ammu.model.RegistrationModel;
 
-public interface UserRepository extends JpaRepository<RegistrationModel, Long> 
+public interface UserRepository extends JpaRepository<LoginModel, Long> 
 {
-	Optional<RegistrationModel> findByEmail(String mail);
+	
 }
