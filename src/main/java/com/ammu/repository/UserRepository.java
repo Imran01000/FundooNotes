de6633308ff5acaf10f94d2b2bcc90cb74223702
Imran01000 <imshaikh01000@gmail.com>
2020-04-19 20:34:59 +0530

    MODIFIED:Removed RegistrationModel.java and Added UserModel with newly added attribute contact_no

--- src/main/java/com/ammu/repository/UserRepository.java
+++ src/main/java/com/ammu/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.ammu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.ammu.model.LoginModel;
-import com.ammu.model.RegistrationModel;
+import com.ammu.model.UserModel;
 
-public interface UserRepository extends JpaRepository<LoginModel, Long> 
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Integer> 
 {
 	
 }
