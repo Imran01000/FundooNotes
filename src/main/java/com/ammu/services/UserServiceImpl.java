4c4ae997867c13589956329ca2f49ace19dabacb
Imran01000 <imshaikh01000@gmail.com>
2020-04-20 13:11:33 +0530

    MODIFIED:Added implementation for login

--- src/main/java/com/ammu/services/UserServiceImpl.java
+++ src/main/java/com/ammu/services/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.ammu.dto.LoginDto;
 import com.ammu.dto.RegistrationDto;
 import com.ammu.model.UserModel;
 import com.ammu.repository.UserRepository;
@@ -17,9 +18,11 @@ public class UserServiceImpl implements UserService
 	@Autowired
 	UserRepository user;
 	
+	//USES MODEL MAPPER CLASS TO TRANSFER PROPERTIES FROM DTO TO ENTITY.
 	ModelMapper mapper = new ModelMapper();
-
-	UserModel userModel = new UserModel(); 
+	
+	//CREATED USER ENTITY OBJECT.
+	UserModel userModel;
 	
 	@Override
 	public Response registration(RegistrationDto registrationDto) 
@@ -30,6 +33,15 @@ public class UserServiceImpl implements UserService
 		return new Response("Details successfully saved" , 200);
 	}
 
-
-
+	@Override
+	public Response login(LoginDto loginDto) 
+	{
+		userModel = user.findByEmail(loginDto.getEmail());
+		
+		if(userModel == null)
+		{
+			return  new Response("Not valid",415);
+		}
+		return new Response("You Logged in", 202);
+	}
 }
