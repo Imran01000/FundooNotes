1311ff4e452eafd935032c347f15249abf1b9e07
Imran01000 <imshaikh01000@gmail.com>
2020-04-20 13:07:06 +0530

    REFACTORED:Replace findByUserName method by findByEmail

--- src/main/java/com/ammu/repository/UserRepository.java
+++ src/main/java/com/ammu/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ammu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.ammu.model.UserModel;
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer> 
 {
-	
+	UserModel findByEmail(String UserName);
+	UserModel findByPassword(String password);
 }
