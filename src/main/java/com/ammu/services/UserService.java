5930f5a02dfe323c91bf53eac1c7181977c0639b
Imran01000 <imshaikh01000@gmail.com>
2020-04-20 15:43:33 +0530

    MODIFIED:Made comments

--- src/main/java/com/ammu/services/UserService.java
+++ src/main/java/com/ammu/services/UserService.java
@@ -1,9 +1,14 @@
 package com.ammu.services;
 
+import com.ammu.dto.ForgetPasswordDto;
+import com.ammu.dto.LoginDto;
 import com.ammu.dto.RegistrationDto;
 import com.ammu.response.Response;
 
 public interface UserService 
 {
+	//DECLARED METHODS.
 	public Response registration(RegistrationDto registrationDto);
+	public Response login(LoginDto loginDto);
+	public Response forgetPassword(ForgetPasswordDto forgetPasswordDto);
 }
