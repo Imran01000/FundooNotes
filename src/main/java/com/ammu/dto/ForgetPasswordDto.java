19ecd0ebacd6cf784f2d9accd45b791a47d1ec48
Imran01000 <imshaikh01000@gmail.com>
2020-04-22 13:41:24 +0530

    MODIFIED:Added validation part

--- src/main/java/com/ammu/dto/ForgetPasswordDto.java
+++ src/main/java/com/ammu/dto/ForgetPasswordDto.java
@@ -1,11 +1,17 @@
 package com.ammu.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class ForgetPasswordDto 
 {
 	//FIELDS DEFINE.
+	@NotNull(message = "Enter email")
+	@Pattern(regexp = "[a-zA-Z0-9]*@[a-zA-Z]+[.][a-zA-Z]+")
 	private String email;
 	
 	//SETTER AND GETTER.
