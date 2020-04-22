4eea08252274c8f27711561ef655124a2db4eccd
Imran01000 <imshaikh01000@gmail.com>
2020-04-22 13:43:22 +0530

    MODIFIED:Added validation part

--- src/main/java/com/ammu/dto/LoginDto.java
+++ src/main/java/com/ammu/dto/LoginDto.java
@@ -1,12 +1,22 @@
 package com.ammu.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoginDto 
 {
 	//DEFINE FIELDS.
+	@NotNull(message = "Enter email")
+	@Pattern(regexp = "[a-zA-Z0-9]*@[a-zA-Z]+[.][a-zA-Z]+")
 	private String email;
+	
+	@NotNull(message = "Enter password")
+	@Pattern(regexp = "[0-9A-Za-z]*")
+	@Size(min = 6 , max = 10)
 	private String password;
 	
 	//SETTER AND GETTER.
