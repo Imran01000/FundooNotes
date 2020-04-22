05996715407f56edf7eb276f18750281e7bbc4a2
Imran01000 <imshaikh01000@gmail.com>
2020-04-22 13:54:57 +0530

    MODIFIED:Made comments

--- src/main/java/com/ammu/repository/UserRepository.java
+++ src/main/java/com/ammu/repository/UserRepository.java
@@ -1,16 +1,13 @@
 package com.ammu.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.ammu.model.UserModel;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Integer> 
+public interface UserRepository extends JpaRepository<UserModel , Integer> 
 {
 	//DEFINE METHODS.
-	UserModel findByEmail(String UserName);
+	UserModel findByEmail(String email);
 	UserModel findByPassword(String password);
 }
