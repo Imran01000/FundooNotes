4b5ce3f25e6e68781f0701e9c8705cd31f25987c
Imran01000 <imshaikh01000@gmail.com>
2020-04-22 13:45:15 +0530

    MODIFIED:Added validation part

--- src/main/java/com/ammu/dto/RegistrationDto.java
+++ src/main/java/com/ammu/dto/RegistrationDto.java
@@ -1,15 +1,36 @@
 package com.ammu.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size; 
 import org.springframework.stereotype.Component;
 
 @Component
 public class RegistrationDto 
 {
 	//DEFINE FIELDS.
+	@NotBlank(message = "Enter first name")
+	@Size(min = 3 , max = 15)
+	@Pattern(regexp = "[A-Z][a-z]*")
 	private String fname;
+	
+	@NotBlank(message = "Enter last name")
+	@Size(min = 3 , max = 15)
+	@Pattern(regexp = "[A-Z][a-z]*")
 	private String lname;
+	
+	@NotNull(message = "Enter email")
+	@Pattern(regexp = "[a-zA-Z0-9]*@[a-zA-Z]+[.][a-zA-Z]+")
 	private String email;
+	
+	@NotNull(message = "enter contact number")
+	@Pattern(regexp = "[7-9][0-9]{9}")
 	private String mobileNo;
+	
+	@NotNull(message = "Enter password")
+	@Pattern(regexp = "[0-9A-Za-z]*")
+	@Size(min = 6 , max = 10)
 	private String password;
 	
 	//SETTER AND GETTER.
