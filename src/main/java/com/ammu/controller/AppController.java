4160142c92458b5cab7bcb66ba5a97723086c4f7
Imran01000 <imshaikh01000@gmail.com>
2020-04-24 18:49:31 +0530

    MODIFIED:Added user verification

--- src/main/java/com/ammu/controller/AppController.java
+++ src/main/java/com/ammu/controller/AppController.java
@@ -1,11 +1,18 @@
 package com.ammu.controller;
 
 import javax.validation.Valid;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ammu.dto.ForgetPasswordDto;
@@ -14,7 +21,10 @@ import com.ammu.dto.RegistrationDto;
 import com.ammu.dto.ResetPasswordDto;
 import com.ammu.repository.UserRepository;
 import com.ammu.response.Response;
+import com.ammu.security.utility.JwtToken;
 import com.ammu.services.UserService;
+
+import jdk.jshell.Snippet.Status;
 
 @RestController   
 @RequestMapping(path = "/user")          // http://localhost:8082/user
@@ -32,34 +42,43 @@ public class AppController
 
 	//RESPONSE TYPE METHOD IN WHICH WE CALLING USER REGISTRATION METHOD.  
 	@PostMapping(path = "/registration")
-	public Response registration(@Valid @RequestBody RegistrationDto registrationDto)
+	public ResponseEntity<Response> registration(@Valid @RequestBody RegistrationDto registrationDto)
 	{
 		response = userService.registration(registrationDto);
-		return response;
-		
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 
 	//RESPONSE TYPE METHOD IN WHICH WE CALLING USER LOGIN METHOD.  
 	@PostMapping(path = "/login")
-	public Response login(@Valid @RequestBody LoginDto loginDto)
+	@ResponseBody
+	public ResponseEntity<Response> login(@Valid @RequestBody LoginDto loginDto)
 	{
 		response = userService.login(loginDto);
-		return response;
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 
 	//RESPONSE TYPE METHOD IN WHICH WE CALLING FORGET PASSWORD METHOD,
 	@PostMapping(path = "/forget-password") 
-	public Response forgetPassword(@Valid @RequestBody ForgetPasswordDto forgetPasswordDto)
+	public ResponseEntity<Response> forgetPassword(@Valid @RequestBody ForgetPasswordDto forgetPasswordDto)
 	{
 		response = userService.forgetPassword(forgetPasswordDto);
-		return response;
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
 	//RESPONSE TYPE METHOD IN WHICH WE CALLING RESET PASSWORD METHOD.
-	@PostMapping(path = "/reset-password")
-	public Response resetPassword(@Valid @RequestBody ResetPasswordDto resetPasswordDto)
+	@PutMapping(path = "/reset-password")
+	public ResponseEntity<Response> resetPassword( @Valid @RequestBody ResetPasswordDto resetPasswordDto)
 	{
 		response = userService.resetPassword(resetPasswordDto);
-		return response;
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
+	
+	@PostMapping(path = "/verification")
+	@ResponseBody
+	public ResponseEntity<Response> userVerification(@RequestParam(name = "id") String id) 
+	{
+
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
+	}
+	
 }
