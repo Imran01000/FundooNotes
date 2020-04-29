dde3d2a85425eb432d0338230e2ec920d69f24a6
Imran01000 <imshaikh01000@gmail.com>
2020-04-30 00:11:16 +0530

    MODIFIED:Made proper indentation

--- src/main/java/com/ammu/services/UserServiceImpl.java
+++ src/main/java/com/ammu/services/UserServiceImpl.java
@@ -1,17 +1,7 @@
 package com.ammu.services;
 
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.ammu.dto.ForgetPasswordDto;
@@ -30,25 +20,21 @@ public class UserServiceImpl implements UserService
 	private UserRepository user;
 
 	@Autowired
-	private JavaMailSender javaMailSender;
-	
-	@Autowired
-	JwtToken jwtToken;
-	
-	@Autowired
 	UserService userService;
+	
+	@Autowired
+	EmailService emailService;
 
-	//USES MODEL MAPPER CLASS TO TRANSFER PROPERTIES FROM DTO TO ENTITY.
 	ModelMapper mapper = new ModelMapper();
 
-	//CREATED USER ENTITY OBJECT.
 	UserModel userModel = new UserModel();
-
+	
 	//IMPLEMENTATION FOR REGISTRATION FORM TO SAVE THE DATA.
 	@Override
 	public Response registration(RegistrationDto registrationDto) 
 	{
-		userService.sendMail(registrationDto.getEmail(), "Do Registration", "click to proceed"+JwtToken.generateToken(registrationDto));
+		emailService.notificationService(registrationDto.getEmail(), "Thanks for registration!", 
+				"Hey thankyou to connect with us hope you are doing good  "+JwtToken.generateToken(registrationDto));
 		mapper.map(registrationDto, userModel);
 		user.save(userModel);
 		return new Response("Details successfully saved" , 200);
@@ -83,7 +69,6 @@ public class UserServiceImpl implements UserService
 	@Override
 	public Response resetPassword(ResetPasswordDto resetPasswordDto)
 	{
-		userService.sendMail(resetPasswordDto.getConfirmPassword(), "reset password" , "click to proceed"+JwtToken.generateToken(resetPasswordDto));
 		userModel = user.findByEmail(resetPasswordDto.getConfirmPassword());
 		if(userModel != null)
 		{
@@ -94,21 +79,5 @@ public class UserServiceImpl implements UserService
 		return new Response("Please provide correct email", 415);
 	}
 
-	@Override
-	public void sendMail(String to, String subject, String text) 
-	{
-		try
-		{
-			SimpleMailMessage message = new SimpleMailMessage(); 
-			message.setTo(to); 
-			message.setSubject(subject); 
-			message.setFrom("is45934@gmail.com");
-			message.setText(text);
-			javaMailSender.send(message);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+	
 }
