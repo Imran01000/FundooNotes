8e7f15cf582ca4de16b994e9ba85897690826638
Imran01000 <imshaikh01000@gmail.com>
2020-04-30 15:31:47 +0530

    MODIFIED:Added ModelMapper bean

--- src/main/java/com/ammu/configuration/Configuration.java
+++ src/main/java/com/ammu/configuration/Configuration.java
@@ -1,11 +1,17 @@
 package com.ammu.configuration;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration 
 {
+	@Bean
+	public ModelMapper modelMapper()
+	{
+		return new ModelMapper();
+	}
 	
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer property()
