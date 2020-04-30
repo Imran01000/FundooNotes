c527fb79eb3e1ad9bf2b1c4216fd86b0325c63c2
Imran01000 <imshaikh01000@gmail.com>
2020-04-30 15:09:48 +0530

    MODIFIED:Added enviroment variables

--- src/main/java/com/ammu/controller/AppController.java
+++ src/main/java/com/ammu/controller/AppController.java
@@ -1,8 +1,6 @@
 package com.ammu.controller;
 
 import javax.validation.Valid;
-
-import org.apache.jasper.tagplugins.jstl.core.Out;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,39 +16,34 @@ import com.ammu.dto.ForgetPasswordDto;
 import com.ammu.dto.LoginDto;
 import com.ammu.dto.RegistrationDto;
 import com.ammu.dto.ResetPasswordDto;
-import com.ammu.repository.UserRepository;
+import com.ammu.exceptionHandling.ResourceNotFoundException;
+import com.ammu.exceptionHandling.UserServiceException;
 import com.ammu.response.Response;
-import com.ammu.security.utility.JwtToken;
 import com.ammu.services.UserService;
 
-import jdk.jshell.Snippet.Status;
 
 @RestController   
 @RequestMapping(path = "/user")          // http://localhost:8082/user
 public class AppController 
 {
-	//USER REPOSITORY AUTOWIRED TO GET THE OBJECT FROM JAVA BEAN CLASS.
-	@Autowired
-	UserRepository userRepository;
-
 	//USER SERVICE AUTOWIRED TO GET THE OBJECT FROM JAVA BEAN CLASS.
 	@Autowired
-	UserService userService;
+	private UserService userService;
 
-	Response response;
-
+	private Response response;
+		
 	//RESPONSE TYPE METHOD IN WHICH WE CALLING USER REGISTRATION METHOD.  
-	@PostMapping(path = "/registration")
-	public ResponseEntity<Response> registration(@Valid @RequestBody RegistrationDto registrationDto)
+	@PostMapping(value = "/registration")
+	public ResponseEntity<Response> registration(@Valid @RequestBody RegistrationDto registrationDto)  																																	
 	{
+		
 		response = userService.registration(registrationDto);
 		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 
 	//RESPONSE TYPE METHOD IN WHICH WE CALLING USER LOGIN METHOD.  
 	@PostMapping(path = "/login")
-	@ResponseBody
-	public ResponseEntity<Response> login(@Valid @RequestBody LoginDto loginDto)
+	public ResponseEntity<Response> login(@Valid @RequestBody LoginDto loginDto) 
 	{
 		response = userService.login(loginDto);
 		return new ResponseEntity<Response>(response, HttpStatus.OK);
