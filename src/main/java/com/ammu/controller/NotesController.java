f481a886514b34002045aa1a4cd3eea75e5b61d3
Imran01000 <imshaikh01000@gmail.com>
2020-05-01 12:34:52 +0530

    MODIFIED:Added required api's

--- src/main/java/com/ammu/controller/NotesController.java
+++ src/main/java/com/ammu/controller/NotesController.java
@@ -1,9 +1,13 @@
 package com.ammu.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ammu.dto.NotesDto;
+import com.ammu.model.NotesModel;
 import com.ammu.response.Response;
 import com.ammu.services.NotesService;
 
@@ -41,5 +46,44 @@ public class NotesController
 	{
 		response = noteService.updateNote(notesDto, id);
 		return new ResponseEntity<Response>(response , HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/showAll")
+	public List<NotesModel> retriveAllNote()
+	{
+		return noteService.retriveAllNote();
+	}
+	
+	@GetMapping(value = "/findByTitle")
+	public ResponseEntity<Response> findByTitle(@RequestParam(name = "title")String data )
+	{
+		response = noteService.findByTitle(data);
+		return new ResponseEntity<Response>(response , HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/findBydata")
+	public ResponseEntity<Response> findByDescription(@RequestParam(name = "description") String data)
+	{
+		response = noteService.findByDescription(data);
+		return new ResponseEntity<Response>(response , HttpStatus.OK);
+		
+	}
+	
+	@GetMapping(path = "/sortByTitle")
+	public List<NotesModel> sortByTitle()
+	{
+		return noteService.sortByTitle();
+	}
+	
+	@GetMapping(path = "/sortByDescription")
+	public List<NotesModel> sortByDescription()
+	{
+		return noteService.sortByDescription();
+	}
+	
+	@GetMapping(path = "/remainder")
+	public LocalDateTime setRemainder()
+	{
+		return noteService.setRemainder();
 	}
 }
