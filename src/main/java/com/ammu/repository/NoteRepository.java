c7d1c5863ef08800140fa27b76ca3c52c6ccd395
Imran01000 <imshaikh01000@gmail.com>
2020-05-01 12:33:29 +0530

    MODIFIED:Added required methods

--- src/main/java/com/ammu/repository/NoteRepository.java
+++ src/main/java/com/ammu/repository/NoteRepository.java
@@ -1,5 +1,7 @@
 package com.ammu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -11,6 +13,19 @@ import com.ammu.model.NotesModel;
 public interface NoteRepository extends CrudRepository<NotesModel , Long >
 {
 	@Modifying
-	@Query(value = "UPDATE user_notes SET title=? , description=? where id=?" ,nativeQuery = true )
-	void updateTitleAndDescription(String title , String description , Long id);
+	@Query(value = "UPDATE user_notes SET title=? , description=? where id=?" , nativeQuery = true )
+	public void updateTitleAndDescription(String title , String description , Long id);
+	
+	public NotesModel findByTitle(String title);
+	
+	public NotesModel findByDescription(String description);
+	
+	@Query(value = "SELECT * FROM user_notes ORDER BY title" , nativeQuery = true)
+	public List<NotesModel> sortByTitle();
+	
+	@Query(value = "SELECT * FROM user_notes ORDER BY description" , nativeQuery = true)
+	public List<NotesModel> sortByDescription();
+
+	
+
 }
