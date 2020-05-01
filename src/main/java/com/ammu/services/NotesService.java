86c33472785ff4e9c454e053499e00029ab710bf
Imran01000 <imshaikh01000@gmail.com>
2020-05-01 12:36:35 +0530

    MODIFIED:Defined required methods

--- src/main/java/com/ammu/services/NotesService.java
+++ src/main/java/com/ammu/services/NotesService.java
@@ -1,6 +1,11 @@
 package com.ammu.services;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.ammu.dto.NotesDto;
+import com.ammu.dto.RemainderDto;
+import com.ammu.model.NotesModel;
 import com.ammu.response.Response;
 
 public interface NotesService
@@ -9,4 +14,10 @@ public interface NotesService
 	public Response addNote(NotesDto notesDto);
 	public Response deleteNote(Long id);
 	public Response updateNote(NotesDto notesDto , Long id);
+	public List<NotesModel> retriveAllNote();
+	public Response findByTitle(String title);
+	public Response findByDescription(String description);
+	public List<NotesModel> sortByTitle();
+	public List<NotesModel> sortByDescription();
+	public LocalDateTime setRemainder();
 }
