ca296420fe634707427578596b383b88f706a3f7
Imran01000 <imshaikh01000@gmail.com>
2020-05-19 18:41:15 +0530

    MODIFIED:Made class methods of optional class type

--- src/main/java/com/ammu/repository/UserRepository.java
+++ src/main/java/com/ammu/repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.ammu.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.ammu.model.UserModel;
+import com.google.common.base.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel , Integer> 
 {
 	//DEFINE METHODS.
-	UserModel findByEmail(String email);
-	UserModel findByPassword(String password);
+	
+	Optional<UserModel> findByEmail(String email);
+	Optional<UserModel> findByPassword(String password);
 }
