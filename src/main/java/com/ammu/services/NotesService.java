ea8b2c939b05ee2502c69ff9ce626d53a8fefd44
Imran01000 <imshaikh01000@gmail.com>
2020-05-23 20:08:41 +0530

    MODIFIED:Made optional type methods

--- src/main/java/com/ammu/services/NotesService.java
+++ src/main/java/com/ammu/services/NotesService.java
@@ -1,23 +1,21 @@
 package com.ammu.services;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import org.springframework.stereotype.Service;
 
 import com.ammu.dto.NotesDto;
-import com.ammu.dto.RemainderDto;
-import com.ammu.model.NotesModel;
 import com.ammu.response.Response;
 
+@Service
 public interface NotesService
 {
 	//DECLARED METHODS.
-	public Response addNote(NotesDto notesDto);
-	public Response deleteNote(Long id);
-	public Response updateNote(NotesDto notesDto , Long id);
-	public List<NotesModel> retriveAllNote();
-	public Response findByTitle(String title);
-	public Response findByDescription(String description);
-	public List<NotesModel> sortByTitle();
-	public List<NotesModel> sortByDescription();
-	public LocalDateTime setRemainder();
+	public Response addNote(NotesDto notesDto , String token); 
+	public Response deleteNote(String token , int id);
+	public Response updateNote(NotesDto notesDto , int id , String token); 
+//	public List<NotesModel> retriveAllNote();
+	public Response findByTitle(String title , String token);
+	public Response findByDescription(String description , String token);
+//	public List<NotesModel> sortByTitle();
+//	public List<NotesModel> sortByDescription();
+//	public LocalDateTime setRemainder();
 }
