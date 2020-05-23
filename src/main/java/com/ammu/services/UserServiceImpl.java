725f353581e7a8ab99c2c257d8a02006a07096d5
Imran01000 <imshaikh01000@gmail.com>
2020-05-23 10:32:33 +0530

    MODIFIED:Added exceptions where required

--- src/main/java/com/ammu/services/UserServiceImpl.java
+++ src/main/java/com/ammu/services/UserServiceImpl.java
@@ -10,7 +10,10 @@ import com.ammu.dto.ForgetPasswordDto;
 import com.ammu.dto.LoginDto;
 import com.ammu.dto.RegistrationDto;
 import com.ammu.dto.ResetPasswordDto;
-import com.ammu.exceptionHandling.UserServiceException;
+import com.ammu.exceptionHandling.ForgetPasswordException;
+import com.ammu.exceptionHandling.LoginException;
+import com.ammu.exceptionHandling.RegistrationException;
+import com.ammu.exceptionHandling.ResetPasswordException;
 import com.ammu.model.UserModel;
 import com.ammu.repository.UserRepository;
 import com.ammu.response.Response;
@@ -23,105 +26,113 @@ public class UserServiceImpl implements UserService
 {
 	@Autowired
 	private UserRepository user;
-	
+
 	@Autowired
 	private EmailService emailService;
-	
+
 	@Autowired
 	private ModelMapper mapper;
-	
+
 	@Autowired
 	private UserModel userModel;
-	
+
 	@Autowired
 	private Environment enviroment;
-	
+
 	//DEFINE VARIABLE TO STORE TOKEN.
 	private String token;
-	
+
 	//DEFINE VARIABLE TO STORE EMAIL.
 	private String email;
 	
 	//IMPLEMENTATION FOR REGISTRATION FORM TO SAVE THE DATA.
 	@Override
-	public Response registration(RegistrationDto registrationDto) throws UserServiceException
+	public Response registration(RegistrationDto registrationDto) 
 	{
 		//URL FOR USER VERFICATION.
 		String urlRegistration = enviroment.getProperty("url.verfication");
-		
-		//TOKON VARIABLE.
-		token = JwtToken.generateToken(registrationDto);
-				
+
 		//CONDITION TO CHECK USER ALREADY PRESENT OR NOT.
 		if(user.findByEmail(registrationDto.getEmail()).isPresent())	
-		{
-			return new Response(enviroment.getProperty("registration.error.text"), enviroment.getProperty("registration.error.code"));
-		}
-		
+			throw new RegistrationException("User is already present", "400");
+	
+
 		//EMAIL SERVICE.
 		emailService.notificationService(registrationDto.getEmail(), enviroment.getProperty("message.gmail.registration"), 
-		enviroment.getProperty("message.gmail.subject")+urlRegistration+token);
-		
+				enviroment.getProperty("message.gmail.subject")+urlRegistration+JwtToken.generateUserToken(userModel.getId()));
+
 		//STORING DATA BY MODEL MAPPER.
-		mapper.map(registrationDto, userModel);
+		 mapper.map(registrationDto, userModel);
 		
-		//DATA SAVED USINF USER SERVICE REFRENCE VARIABLE.
+		
+		//DATA SAVED USING USER SERVICE REFRENCE VARIABLE.
 		user.save(userModel);
-		
+
 		return new Response( enviroment.getProperty("registration.success.text") , enviroment.getProperty("registration.success.code"));
 	}
- 
+
 	//IMPLEMENTATION OF METHOD FOR LOGIN.
 	@Override
-	public Response login(LoginDto loginDto) throws UserServiceException
+	public Response login(LoginDto loginDto)
 	{
 		//CONDITION TO CHECK USER EMAIL AMD PASSWORD.
 		if(user.findByEmail(loginDto.getEmail()).isPresent() && user.findByPassword(loginDto.getPassword()).isPresent())
 		{
-			return new Response(enviroment.getProperty("login.success.text") , enviroment.getProperty("login.success.code"));
+			token = JwtToken.generateUserToken(userModel.getId());
+			return new Response(enviroment.getProperty("login.success.text") , token);
 		}
-		return  new Response(enviroment.getProperty("login.error.text") , enviroment.getProperty("login.error.code"));
+		else if(!user.findByEmail(loginDto.getEmail()).isPresent())
+
+		{
+			throw new LoginException("User is null", "400");
+		}
+
+			return  new Response(enviroment.getProperty("login.error.text") , enviroment.getProperty("login.error.code"));
 
 	}
 
 	//IMPLEMENTATION OF METHOD FOR FORGET PASSWORD.
 	@Override
-	public Response forgetPassword(ForgetPasswordDto forgetPasswordDto) throws UserServiceException
+	public Response forgetPassword(ForgetPasswordDto forgetPasswordDto)
 	{
 		//URL FOR USER RESET PASSWORD.
 		String urlForgetPassword = enviroment.getProperty("url.forgetpassword");
-		
+
 		//VARIABLE TO STORE TOKEN. 
-		token = JwtToken.generateToken(forgetPasswordDto);
-		
+		token = JwtToken.generateUserToken(userModel.getId());
+
 		//STORING EMAIL.
 		email = forgetPasswordDto.getEmail();
-		
+
 		//CONDTION TO CHECK THE VALIDATE USER EMAIL.
 		if(user.findByEmail(forgetPasswordDto.getEmail()).isPresent())
 		{
 			//SENDED MAIL SERVICE TO RESPECTIVE USER.
 			emailService.notificationService(forgetPasswordDto.getEmail(), enviroment.getProperty("message.gmail.forget"), 
 					urlForgetPassword+token);
-			
+
 			return new Response(enviroment.getProperty("forget.success.text"), enviroment.getProperty("forget.success.code"));
 		}
-		
+		else if(!user.findByEmail(forgetPasswordDto.getEmail()).isPresent())
+		{
+			throw new ForgetPasswordException("your email is not valid", "400");
+		}
+
 		return new Response(enviroment.getProperty("forget.error.text"), enviroment.getProperty("forget.error.code"));
 	}
 
 	//IMPLEMENTATION OF METHOD FOR RESET PASSWORD.
 	@Override
-	public Response resetPassword(ResetPasswordDto resetPasswordDto , String emailToken) throws UserServiceException
+	public Response resetPassword(ResetPasswordDto resetPasswordDto , String emailToken) 
 	{	
 		System.out.println(token);
 		System.out.println(emailToken);
-		
+
 		if(emailToken.equals(token))
 		{
 			//STORING EMAIL.
 			Optional<UserModel> userEmail = user.findByEmail(email);
-			
+
 			//CONDITION TO CHECK WHEATHER THE USER IS PRESENT.
 			if(userEmail.isPresent())
 			{
@@ -130,15 +141,15 @@ public class UserServiceImpl implements UserService
 				{
 					//SET PASSWORD FOR RESPECTIVE EMAIL.
 					userEmail.get().setPassword(resetPasswordDto.getNewPassword());
-					
+
 					//SAVE THE DATA TO DATABASE.
 					user.save(userEmail.get());
-					
+
 					return new Response(enviroment.getProperty("reset.success.text"), enviroment.getProperty("reset.success.code"));
 				}
 				else
 				{
-					return new Response(enviroment.getProperty("reset.error.notMatch.text"), enviroment.getProperty("reset.error.notMatch.code"));
+					throw new ResetPasswordException(enviroment.getProperty("reset.error.notMatch.text"), enviroment.getProperty("reset.error.notMatch.code"));
 				}
 			}
 		}
@@ -153,10 +164,10 @@ public class UserServiceImpl implements UserService
 		{
 			//METHOD TO NOTIFY THAT USER IS VERFIED.
 			userModel.setVerified(true);
-			
+
 			//SAVED USERMODEL ENTITY.
 			user.save(userModel);
-			
+
 			return new Response(enviroment.getProperty("verification.success.text"), enviroment.getProperty("verification.success.code"));
 		} 
 		return new Response(enviroment.getProperty("verification.error.text"), enviroment.getProperty("verification.error.code"));
