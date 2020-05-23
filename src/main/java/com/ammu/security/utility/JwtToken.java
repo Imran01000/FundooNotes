34b68ec692c66586921068a6f14bb48dfd67c051
Imran01000 <imshaikh01000@gmail.com>
2020-05-23 20:06:44 +0530

    MODIFIED:Modified jwt token code

--- src/main/java/com/ammu/security/utility/JwtToken.java
+++ src/main/java/com/ammu/security/utility/JwtToken.java
@@ -7,37 +7,35 @@ import java.util.Map;
 import com.ammu.dto.ForgetPasswordDto;
 import com.ammu.dto.LoginDto;
 import com.ammu.dto.RegistrationDto;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import com.auth0.jwt.interfaces.Verification;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JwtToken 
 {
-	private static String secretKey;
+	private static String secretKey = "SECRETKEY";
 	
-	public static final long JWT_TOKEN_VALIDITY = 1000*60*5;
+	 public static String generateUserToken(int id) {
+		 	secretKey =  "SECRETKEY";
+	        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+	        String token = JWT.create().withClaim("ID", id).sign(algorithm);
+	        return token;
+	    }
 
-	public static String generateToken(RegistrationDto registrationDto)
-	{
-		Map<String , Object> claims = new HashMap<>();
-		return doGenerateToken(claims , registrationDto.getEmail());
-	}
-	
-	public static String generateToken(ForgetPasswordDto forgetPasswordDto)
-	{
-		Map<String , Object> claims = new HashMap<>();
-		return doGenerateToken(claims , forgetPasswordDto.getEmail());
-	}
-	
-	public static String generateToken(LoginDto loginDto)
-	{
-		Map<String , Object> claims = new HashMap<>();
-		return doGenerateToken(claims , loginDto.getEmail());
-	}
-	
-	public static String doGenerateToken(Map<String, Object> claims, String email) 
-	{
-		return Jwts.builder().setClaims(claims).setSubject(email).setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis()+JWT_TOKEN_VALIDITY)).signWith(SignatureAlgorithm.HS512, secretKey)
-				.compact();
-	}
+	    public static int retrieveIdFromToken(String token) {
+	    	secretKey =  "SECRETKEY";
+	        Verification verification;
+	        verification = JWT.require(Algorithm.HMAC256(secretKey));
+	        JWTVerifier jwtverifier = verification.build();
+	        DecodedJWT decodedjwt = jwtverifier.verify(token);
+	        Claim claim = decodedjwt.getClaim("ID");
+	        int id = claim.asInt();
+	        return id;
+	    }
 }
