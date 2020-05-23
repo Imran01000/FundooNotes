a782d7060c96b745c08ecb4990ab36a8d9afc394
Imran01000 <imshaikh01000@gmail.com>
2020-05-23 20:13:03 +0530

    MODIFIED:Made proper indentation

--- src/main/java/com/ammu/repository/UserRepository.java
+++ src/main/java/com/ammu/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ammu.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.ammu.model.UserModel;
-import com.google.common.base.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel , Integer> 
