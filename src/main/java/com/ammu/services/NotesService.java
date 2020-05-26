c184e7f5c5d083fd60b3b5eb23fb8bcbd05067d9
Imran01000 <imshaikh01000@gmail.com>
2020-05-26 18:12:39 +0530

    MODIFIED:Added trash and untrash method

--- src/main/java/com/ammu/services/NotesService.java
+++ src/main/java/com/ammu/services/NotesService.java
@@ -1,8 +1,12 @@
 package com.ammu.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.ammu.dto.NotesDto;
+import com.ammu.dto.ReminderDto;
+import com.ammu.model.NotesModel;
 import com.ammu.response.Response;
 
 @Service
@@ -10,12 +14,13 @@ public interface NotesService
 {
 	//DECLARED METHODS.
 	public Response addNote(NotesDto notesDto , String token); 
-	public Response deleteNote(String token , int id);
-	public Response updateNote(NotesDto notesDto , int id , String token); 
-//	public List<NotesModel> retriveAllNote();
+	public Response deleteNote(String token , int noteId);
+	public Response updateNote(NotesDto notesDto , int noteId , String token); 
+	public List<NotesModel> retriveAllNote(String token);
 	public Response findByTitle(String title , String token);
 	public Response findByDescription(String description , String token);
-//	public List<NotesModel> sortByTitle();
-//	public List<NotesModel> sortByDescription();
-//	public LocalDateTime setRemainder();
+	public Response trashAndUnTrash(int noteId , String token);
+	public List<NotesModel> sortByTitle(String token);
+	public List<NotesModel> sortByDescription(String token);
+	public Response setReminder(ReminderDto remainderDto , String token);
 }
