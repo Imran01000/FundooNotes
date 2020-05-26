8596ac29ae6f4e8ad2c359cd81d560ead68638b6
Imran01000 <imshaikh01000@gmail.com>
2020-05-26 18:09:53 +0530

    MODIFIED:Removed all queries

--- src/main/java/com/ammu/repository/NoteRepository.java
+++ src/main/java/com/ammu/repository/NoteRepository.java
@@ -1,9 +1,12 @@
 package com.ammu.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ammu.model.NotesModel;
@@ -13,9 +16,10 @@ import com.ammu.model.UserModel;
 @Repository
 public interface NoteRepository extends JpaRepository<NotesModel , Integer>
 {
-	
 	 Optional<NotesModel> findByTitleAndUserModel(String title , UserModel userModel);
 	 Optional<NotesModel> findByIdAndUserModel(int noteId , UserModel userModel);
 	 Optional<NotesModel> findByTitleAndDescription( String title , String description);
 	 Optional<NotesModel> findByDescription(String description);
+	 List<NotesModel> findByOrderByTitleAsc();
+	 List<NotesModel> findByOrderByDescriptionAsc();
 }
