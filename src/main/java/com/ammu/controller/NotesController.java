6ae2b537f962fb2f3e6b4fe7ec514c4d0ff752ae
Imran01000 <imshaikh01000@gmail.com>
2020-06-07 20:48:15 +0530

    MODIFIED:Added color method to controller

--- src/main/java/com/ammu/controller/NotesController.java
+++ src/main/java/com/ammu/controller/NotesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.ammu.services.NotesService;
 
 @RestController
 @RequestMapping(path = "/user")
+@CrossOrigin(origins = "*")
 public class NotesController 
 {
 	@Autowired
@@ -31,66 +33,78 @@ public class NotesController
 	
 	private Response response;
 	
-	@PostMapping("/addNote")
-	public ResponseEntity<Response> addUserNotes(@RequestBody NotesDto notesDto ,@RequestHeader("token") String token)
+	@PostMapping("/addnote")
+	public ResponseEntity<Response> addUserNotes(@RequestBody NotesDto notesDto, @RequestHeader("token") String token)
 	{
 		response = noteService.addNote(notesDto , token);		
 		return new ResponseEntity<Response>(response, HttpStatus.OK);	
 	}
 	
-	@DeleteMapping("/deleteNote/{id}")
-	public ResponseEntity<Response> deleteNotes( @RequestHeader("token") String token , @PathVariable("id") int noteId)
+	@DeleteMapping("/deletenote/{id}")
+	public ResponseEntity<Response> deleteNotes( @RequestHeader("token") String token, @PathVariable("id") int noteId)
 	{
 		response = noteService.deleteNote(token , noteId);
 		return new ResponseEntity<Response>(response , HttpStatus.OK);
 	}
 	
-	@PutMapping(path = "/updateNote/{id}")
-	public ResponseEntity<Response> updateUserNotes(@PathVariable(name = "id") int id , NotesDto notesDto
+	@PutMapping(path = "/updatenote/{id}")
+	public ResponseEntity<Response> updateUserNotes(@PathVariable(name = "id") int id, NotesDto notesDto
 			,@RequestHeader(name = "token") String token)
 	{
-		response = noteService.updateNote(notesDto, id , token);
-		return new ResponseEntity<Response>(response , HttpStatus.OK);
+		response = noteService.updateNote(notesDto, id, token);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
-	@GetMapping(path = "/showAll")
+	@GetMapping(path = "/showall")
 	public List<NotesModel> retriveAllNote(@RequestHeader("token")String token) 
 	{
 		return noteService.retriveAllNote(token);
 	}
 	
-	@GetMapping(value = "/findByTitle")
-	public ResponseEntity<Response> findByTitle(@RequestParam(name = "title")String data , @RequestHeader("token") String token)
+	@GetMapping(value = "/findbytitle")
+	public ResponseEntity<Response> findByTitle(@RequestParam(name = "title")String data, @RequestHeader("token") String token)
 	{
-		
-		response = noteService.findByTitle(data , token);
-		return new ResponseEntity<Response>(response , HttpStatus.OK);
+		response = noteService.findByTitle(data, token);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
-	@GetMapping(path = "/findByDescription")
-	public ResponseEntity<Response> findByDescription(@RequestParam(name = "description") String data , @RequestHeader("token") String token)
+	@GetMapping(path = "/findbydescription")
+	public ResponseEntity<Response> findByDescription(@RequestParam(name = "description") String data, @RequestHeader("token") String token)
 	{
-		response = noteService.findByDescription(data , token);
-		return new ResponseEntity<Response>(response , HttpStatus.OK);
-		
+		response = noteService.findByDescription(data, token);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
-	@GetMapping(path = "/sortByTitle")
+	@GetMapping(path = "/sortbytitle")
 	public List<NotesModel> sortByTitle(@RequestHeader("token") String token) 
 	{
 		return noteService.sortByTitle(token);
 	}
 	
-	@GetMapping(path = "/sortByDescription")
+	@GetMapping(path = "/sortbydescription")
 	public List<NotesModel> sortByDescription(@RequestHeader("token") String token) 
 	{
 		return noteService.sortByDescription(token);
 	}
 	
-	@PutMapping(path = "/remainder")
-	public ResponseEntity<Response> setRemainder(ReminderDto reminderDto , @RequestHeader("token") String token)
+	@PutMapping(path = "/archive/{noteId}")
+	public ResponseEntity<Response> setArchive(@PathVariable int noteId ,@RequestHeader("token") String token)
+	{
+		response = noteService.archive(noteId, token);
+		return new ResponseEntity<Response>(response , HttpStatus.OK);
+	}
+	
+	@PutMapping(path = "/reminder")
+	public ResponseEntity<Response> setRemainder(ReminderDto reminderDto, @RequestHeader("token") String token)
 	{
 		response = noteService.setReminder(reminderDto, token);
+		return new ResponseEntity<Response>(response , HttpStatus.OK);
+	}
+	
+	@PutMapping(path = "/color/{id}")
+	public ResponseEntity<Response> setColor(@RequestParam String color, @PathVariable int id, @RequestHeader("token") String token)
+	{
+		response = noteService.addColor(token, id, color);
 		return new ResponseEntity<Response>(response , HttpStatus.OK);
 	}
 }
