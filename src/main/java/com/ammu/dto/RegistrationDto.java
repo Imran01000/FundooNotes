23c5c4eade557a1786841f871ccb87b708a7b042
Imran01000 <imshaikh01000@gmail.com>
2020-06-15 14:38:36 +0530

    MODIFIED:Removed @component

--- src/main/java/com/ammu/dto/RegistrationDto.java
+++ src/main/java/com/ammu/dto/RegistrationDto.java
@@ -4,9 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size; 
-import org.springframework.stereotype.Component;
 
-@Component
 public class RegistrationDto 
 {
 	//DEFINE FIELDS.
@@ -64,6 +62,4 @@ public class RegistrationDto
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 }
