8372402472190870c2366c86efe56d8f1606d6de
Imran01000 <imshaikh01000@gmail.com>
2020-06-15 14:36:44 +0530

    MODIFIED:Removed @component

--- src/main/java/com/ammu/dto/ForgetPasswordDto.java
+++ src/main/java/com/ammu/dto/ForgetPasswordDto.java
@@ -2,11 +2,9 @@ package com.ammu.dto;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class ForgetPasswordDto 
 {
 	//FIELDS DEFINE.
